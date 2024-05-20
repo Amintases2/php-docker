4fc87f4b3e04d54da10dad3f1ee566b3e3494f6c
Amintases <gabitov234567@mail.ru>
2024-05-20 15:07:21 +0400

    add logstash + jdbc

--- src/public/index.php
+++ src/public/index.php
@@ -3,24 +3,15 @@
 define('BASE_PATH', dirname(__DIR__));
 require_once BASE_PATH . '/vendor/autoload.php';
 
-
-
-//dd($pdo);
-
-
-
-
-
 $client = Elastic\Elasticsearch\ClientBuilder::create()
     ->setHosts(['elasticsearch:9200'])
     ->build();
 
 $params = [
-    'index' => 'users',
-    'firstname' => 'Alex'
+    'index' => 'my_idx',
 ];
 
-$response = $client->search()->asArray();
+$response = $client->search($params)->asArray();
 
 dd($response);
 
