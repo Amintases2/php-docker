beca986593ff5a2b8b2602614f33ffe029957558
Marat Gabitov <maratgabitov@MacBook-Pro-Marat.local>
2024-05-30 07:25:08 +0400

    handle router exception

--- src/framework/Routing/Router.php
+++ src/framework/Routing/Router.php
@@ -2,13 +2,25 @@
 
 namespace Framework\Routing;
 
+use FastRoute\Dispatcher;
 use Framework\Http\Request;
 use FastRoute\RouteCollector;
+use Framework\Http\Exceptions\MethodNotAllowedException;
+use Framework\Http\Exceptions\NotFoundException;
+
 use function FastRoute\simpleDispatcher;
 
 class Router implements RouterInterface
 {
     public function dispatch(Request $request): array
+    {
+        [[$controller, $method], $vars] = $this->extractRouteInfo($request);
+
+        return [[new $controller, $method], $vars];
+    }
+
+
+    private function extractRouteInfo(Request $request)
     {
         $dispatcher = simpleDispatcher(function (RouteCollector $collector) {
 
@@ -23,8 +35,17 @@ class Router implements RouterInterface
             $request->getPath()
         );
 
-        [$status, [$controller, $method], $vars] = $routeInfo;
+        switch ($routeInfo[0]) {
+            case Dispatcher::FOUND:
+                return [$routeInfo[1], $routeInfo[2]];
 
-        return [[new $controller, $method], $vars];
+            case Dispatcher::METHOD_NOT_ALLOWED:
+                $allowedMethods = implode(', ', $routeInfo[1]);
+
+                throw new MethodNotAllowedException("Supported HTTP Methods: $allowedMethods");
+
+            default:
+                throw new NotFoundException('Route Not Found');
+        }
     }
 }
