b9969fcf3e1ecf8234cad2fbd605f6eee1363583
Marat Gabitov <maratgabitov@MacBook-Pro-Marat.local>
2024-06-05 20:42:09 +0400

    add fn in route

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,8 +3,12 @@
 use Framework\Routing\Route;
 use App\Controllers\HomeController;
 use App\Controllers\PostController;
+use Framework\Http\Response;
 
 return [
     Route::get('/', [HomeController::class, 'index']),
     Route::get('/posts/{id:\d+}', [PostController::class, 'show']),
+    Route::get('/hi/{name}', function (string $name) {
+        return new Response("hello, {$name}");
+    }),
 ];
