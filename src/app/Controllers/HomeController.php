c87e4cc63d0f2b22278f711d20187a8867bd648b
Marat Gabitov <maratgabitov@MacBook-Pro-Marat.local>
2024-06-16 20:26:31 +0400

    add twig

--- src/app/Controllers/HomeController.php
+++ src/app/Controllers/HomeController.php
@@ -4,17 +4,20 @@ namespace App\Controllers;
 
 use App\Services\TestService;
 use PFW\Framework\Http\Response;
+use Twig\Environment;
 
 class HomeController
 {
-    public function __construct(private readonly TestService $service)
-    {
+    public function __construct(
+        private readonly TestService $service,
+        private readonly Environment $twig
+    ) {
     }
 
     public function index(): Response
     {
         $content = $this->service->sayHello();
-
+        dd($this->twig);
         return new Response($content, 200, []);
     }
 }
--- src/config/services.php
+++ src/config/services.php
@@ -8,27 +8,37 @@ use PFW\Framework\Http\Kernel;
 use PFW\Framework\Routing\Router;
 use PFW\Framework\Routing\RouterInterface;
 use Symfony\Component\Dotenv\Dotenv;
-
-// Applications parametrs
-
-$dotenv = new Dotenv();
-$dotenv->load(BASE_PATH . '/.env');
-
-$routes = include BASE_PATH . '/routes/web.php';
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 
 $container = new Container();
-
 $container->delegate(new ReflectionContainer(true));
 
+// ENV
+$dotenv = new Dotenv();
+$dotenv->load(BASE_PATH . '/.env');
 $appEnv = $_ENV['APP_ENV'] ?? 'local';
 $container->add('APP_ENV', new StringArgument($appEnv));
 
+// ROUTES
+$routes = include BASE_PATH . '/routes/web.php';
 $container->add(RouterInterface::class, Router::class);
 $container->add(Kernel::class)
     ->addArgument($container)
     ->addArgument(RouterInterface::class);
-
 $container->extend(RouterInterface::class)
     ->addMethodCall('registerRoutes', ['routes' => new ArrayArgument($routes)]);
+
+// VIEWS
+$views = BASE_PATH . '/views';
+$container->addShared('twig-loader', FilesystemLoader::class)
+    ->addArgument(new StringArgument($views));
+$container->addShared(Environment::class)
+    ->addArgument('twig-loader');
+
+
+// Applications parametrs
+
+
 
 return $container;
