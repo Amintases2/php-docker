2d7b405a7ef0fda8ea5f79bf68bb0dee924373ea
Marat Gabitov <maratgabitov@MacBook-Pro-Marat.local>
2024-06-16 19:37:07 +0400

    mods

--- src/config/services.php
+++ src/config/services.php
@@ -1,19 +1,27 @@
 <?php
 
 use League\Container\Argument\Literal\ArrayArgument;
+use League\Container\Argument\Literal\StringArgument;
 use League\Container\Container;
 use League\Container\ReflectionContainer;
 use PFW\Framework\Http\Kernel;
 use PFW\Framework\Routing\Router;
 use PFW\Framework\Routing\RouterInterface;
+use Symfony\Component\Dotenv\Dotenv;
 
 // Applications parametrs
+
+$dotenv = new Dotenv();
+$dotenv->load(BASE_PATH . '/.env');
 
 $routes = include BASE_PATH . '/routes/web.php';
 
 $container = new Container();
 
 $container->delegate(new ReflectionContainer(true));
+
+$appEnv = $_ENV['APP_ENV'] ?? 'local';
+$container->add('APP_ENV', new StringArgument($appEnv));
 
 $container->add(RouterInterface::class, Router::class);
 $container->add(Kernel::class)
--- src/framework/src/Http/Kernel.php
+++ src/framework/src/Http/Kernel.php
@@ -10,22 +10,39 @@ use PFW\Framework\Routing\RouterInterface;
 
 class Kernel
 {
+	private string $appEnv;
+
 	public function __construct(
 		private Container $container,
 		private RouterInterface $router
 	) {
+		$this->appEnv = $container->get('APP_ENV');
 	}
 
 	public function handle(Request $request): Response
 	{
 		try {
+			throw new Exception('Some Error');
 			[$routeHandler, $vars] = $this->router->dispatch($request, $this->container);
 
 			$response = call_user_func_array($routeHandler, $vars);
-		} catch (HttpException $exception) {
-			$response = new Response($exception->getMessage(), $exception->getStatusCode());
+		} catch (Exception $exception) {
+			$response = $this->createExceptionResponse($exception);
 		}
 
 		return $response;
+	}
+
+	private function createExceptionResponse(Exception $e): Response
+	{
+		if (in_array($this->appEnv, ['local', 'testing', 'dev'])) {
+			throw $e;
+		}
+
+		if ($e instanceof HttpException) {
+			return new Response($e->getMessage(), $e->getStatusCode());
+		}
+
+		return new Response('Server error', 500);
 	}
 }
