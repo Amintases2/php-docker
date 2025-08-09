eca957603352ef05db66836b3b46220d06a9b2e5
unknown <gabitov234567@mail.ru>
2025-08-09 17:49:35 +0400

    s fix

--- src/database/migrations/001.php
+++ src/database/migrations/001.php
@@ -1,1 +1,26 @@
 <?php
+
+use Doctrine\DBAL\Types\Types;
+
+return new class
+{
+    public function up($scheme): void
+    {
+        $table = $scheme->createTable('posts');
+        $table->addColumn('id', Types::INTEGER, [
+            'unsigned' => true,
+            'autoincrement' => true
+        ]);
+        $table->addColumn('title', Types::STRING);
+        $table->addColumn('body', Types::TEXT);
+        $table->addColumn('created_at', Types::DATETIME_IMMUTABLE, [
+            'default' => 'CURRENT_TIMESTAMP'
+        ]);
+        $table->setPrimaryKey(['id']);
+    }
+
+    public function down(): void
+    {
+        // do nothing
+    }
+};
